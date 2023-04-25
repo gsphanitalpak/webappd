72f9d65563e414e52efb936ae8a9320065081a9f
Gandhala Santhosh Phanitalpak <gsanthoshphanitalpak@gmail.com>
2023-04-25 22:05:27 +0530

    edited main.js

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+      <main>
+
+      </main>
+      <footer>
+
+      </footer>
+    </div>
+  );
+}
+
+export default App;
